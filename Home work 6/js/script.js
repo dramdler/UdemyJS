e939a1d2281860acffa278fa0f0c4eb1db181184
dramdler <dramdler@gmail.com>
2020-04-28 17:39:58 +0300

    home work 6

---
+++ Home work 6/js/script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('.menu');
+
+//меняем местами
+menu.insertBefore(menu.children[2], menu.children[1])
+
+//добавляем пункт меню
+let menuItem = document.createElement('li');
+menuItem.className = ('menu-item');
+menuItem.innerHTML = ('пятый');
+
+menu.appendChild(menuItem);
+
+//меняем фон
+document.body.style.background = "url(./img/apple_true.jpg)" ;
+
+//меняем заголовок
+document.querySelector('#title').innerHTML = 'Мы продаем только подлинную технику Apple';
+
+//удаляем рекламу
+document.querySelector('.adv').remove();
+
+//отношение к технике apple 
+document.querySelector('#prompt').innerHTML = prompt('Опишите ваше отношение к технике APPLE ');LikeApple;
